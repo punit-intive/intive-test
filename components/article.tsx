a1bc471e18165ae56c1233c22c5be692887efc24
Philipp Eckel <philipp.eckel@intive.com>
2024-04-23 10:40:54 +0200

    feat: adjust article page and adding prettier setup

--- components/article.tsx
+++ components/article.tsx
@@ -13,7 +13,7 @@ import {
 } from '@contentful/live-preview/react';
 import { BlogProps } from '@/lib/contentful/api';
 import { ContentfulLivePreview } from '@contentful/live-preview';
-import { BLOCKS, INLINES } from "@contentful/rich-text-types";
+import { BLOCKS, INLINES } from '@contentful/rich-text-types';
 
 function renderOptions(links: any) {
   // create an asset map
@@ -24,71 +24,71 @@ function renderOptions(links: any) {
   }
 
   return {
-
     renderNode: {
       [BLOCKS.EMBEDDED_ASSET]: (node: any) => {
         // find the asset in the assetMap by ID
         const asset = assetMap.get(node.data.target.sys.id);
 
         // render the asset accordingly
-        return (
-          <img src={asset.url} alt={asset.description} />
-        );
+        return <img src={asset.url} alt={asset.description} />;
       },
     },
   };
 }
-
 
 export const Blog = ({ blog }: { blog: BlogProps }) => {
   const updatedBlog = useContentfulLiveUpdates(blog);
   const inspectorProps = useContentfulInspectorMode({ entryId: blog.sys.id });
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
-      <section className="w-full">
-        <div className="container space-y-12 px-4 md:px-6">
-          <div className="space-y-4">
+    <div className='flex min-h-screen flex-col items-center justify-between bg-white p-24'>
+      <section className='w-full'>
+        <div className='container space-y-12 px-4 md:px-6'>
+          <div className='space-y-4'>
             <h1
-              className="text-4xl font-bold tracking-tighter sm:text-5xl"
+              className='text-4xl font-bold tracking-tighter sm:text-5xl'
               {...inspectorProps({ fieldId: 'title' })}
             >
               {updatedBlog.title}
             </h1>
-            <div className="flex justify-between flex-col md:flex-row">
-              <p
-                className="max-w-[900px] text-zinc-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-zinc-400"
-                {...inspectorProps({ fieldId: 'summary' })}
-              >
-                {updatedBlog.summary}
-              </p>
-              <p
-                className="text-zinc-500 md:text-lg/relaxed lg:text-sm/relaxed xl:text-lg/relaxed dark:text-zinc-400 italic"
-                {...inspectorProps({ fieldId: 'author' })}
-              >
-                by: {updatedBlog.author}
-              </p>
+            <div
+              className='text-zinc-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed'
+              {...inspectorProps({ fieldId: 'author' })}
+            >
+              By {updatedBlog.author}
             </div>
           </div>
-          <div className="space-y-8 lg:space-y-10">
+          <div className='space-y-8 lg:space-y-10'>
             <Image
-              alt="Blog Image"
-              className="aspect-video w-full overflow-hidden rounded-xl object-cover"
-              height="365"
+              alt='Blog Image'
+              className='aspect-video w-full overflow-hidden rounded-xl object-cover'
+              height='365'
               src={updatedBlog.heroImage?.url as string}
-              width="650"
+              width='650'
               {...ContentfulLivePreview.getProps({
                 assetId: updatedBlog.heroImage?.sys.id || '',
                 fieldId: 'file',
               })}
             />
-            <div className="space-y-4 md:space-y-6">
-              <div className="space-y-2">
+            {/* <div className="space-y-8 lg:space-y-10"> */}
+            <div className='flex flex-col justify-between md:flex-row'>
+              <p
+                className='max-w-[900px] text-zinc-500 md:text-2xl lg:text-3xl xl:text-4xl dark:text-zinc-400'
+                {...inspectorProps({ fieldId: 'summary' })}
+              >
+                {updatedBlog.summary}
+              </p>
+            </div>
+            <div className='space-y-4 md:space-y-6'>
+              <div className='space-y-2'>
                 <div
-                  className="prose max-w-[900px] text-zinc-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-zinc-400"
+                  className='prose max-w-[900px] text-zinc-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-zinc-400'
                   {...inspectorProps({ fieldId: 'details' })}
                 >
-                  {documentToReactComponents(updatedBlog.details.json, renderOptions(updatedBlog.details.links))}
+                  {documentToReactComponents(
+                    updatedBlog.details.json,
+                    renderOptions(updatedBlog.details.links)
+                  )}
                 </div>
               </div>
             </div>
